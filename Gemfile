c5711f65b920ed45a4e971071b59faed46888cb3
Uwe Eisele <uwe.eisele@online.de>
2017-10-31 20:28:49 +0100

    Test commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+group :test do
+  gem 'test-kitchen'
+  gem 'kitchen-docker'
+  gem 'kitchen-ansible'
+  gem 'serverspec'
+end
---
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'openjdk::default' do
+
+  ## serverspec tests go here
+  #
+  # describe package('ruby2.2') do
+  #   it { should be_installed.by('apt') }
+  # end
+
+end
